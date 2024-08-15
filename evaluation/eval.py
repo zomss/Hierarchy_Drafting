cfae211ca2ec5e13814efb439f3904f0abd6a5a3
zomss <nelllpic@kaist.ac.kr>
2024-08-15 18:50:40 +0900

    Add files

---
+++ evaluation/eval.py
@@ -0,0 +1,289 @@
+"""Generate answers with local models.
+
+Usage:
+python3 gen_model_answer.py --model-path lmsys/fastchat-t5-3b-v1.0 --model-id fastchat-t5-3b-v1.0
+"""
+# adapted from fastchat: https://github.com/lm-sys/FastChat/blob/main/fastchat/llm_judge/gen_model_answer.py
+
+import json
+import os
+import time
+import torch
+import numpy as np
+import shortuuid
+
+from fastchat.llm_judge.common import load_questions
+from fastchat.model import get_conversation_template
+from tqdm import tqdm
+
+system_prompt_mapping = {
+    "Llama" : "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\'t know the answer to a question, please don\'t share false information."
+}
+
+def run_eval(
+        model,
+        tokenizer,
+        forward_func,
+        model_id,
+        question_file,
+        question_begin,
+        question_end,
+        answer_file,
+        max_new_tokens,
+        num_choices,
+        num_gpus_per_model,
+        num_gpus_total,
+        **kwargs,
+):
+    questions = load_questions(question_file, question_begin, question_end)
+
+    # Split the question file into `num_gpus` files
+    assert num_gpus_total % num_gpus_per_model == 0
+    use_ray = num_gpus_total // num_gpus_per_model > 1
+
+    if use_ray:
+        import ray
+        ray.init()
+        get_answers_func = ray.remote(num_gpus=num_gpus_per_model)(
+            get_model_answers
+        ).remote
+    else:
+        get_answers_func = get_model_answers
+
+    chunk_size = len(questions) // (num_gpus_total // num_gpus_per_model)  # // 2
+    ans_handles = []
+    for i in range(0, len(questions), chunk_size):
+        ans_handles.append(
+            get_answers_func(
+                model,
+                tokenizer,
+                forward_func,
+                model_id,
+                questions[i: i + chunk_size],
+                answer_file,
+                max_new_tokens,
+                num_choices,
+                **kwargs,
+            )
+        )
+
+    if use_ray:
+        ray.get(ans_handles)
+
+
+@torch.inference_mode()
+def get_model_answers(
+        model,
+        tokenizer,
+        forward_func,
+        model_id,
+        questions,
+        answer_file,
+        max_new_tokens,
+        num_choices,
+        system_prompt="default",
+        **kwargs,
+):
+
+    model.eval()
+    print('Check model training state:', model.training)
+
+    cuda_visible_devices = os.environ.get('CUDA_VISIBLE_DEVICES')
+    print('CUDA VISIBLE DEVICES:', cuda_visible_devices)
+
+    question = questions[0]
+
+    # warmup
+    for _ in range(3):
+        torch.manual_seed(0)
+        conv = get_conversation_template("vicuna")
+
+        # conv = get_conversation_template("Llama-2-7b")
+        # conv.set_system_message(system_message=system_prompt_mapping["Llama"])
+        
+        turns = []
+        steps = []
+        new_tokens = []
+        wall_time = []
+        for j in range(len(question["turns"])):
+            qs = question["turns"][j]
+            conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
+            conv.stop_str = "</s>"
+            prompt = conv.get_prompt()
+            inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
+            input_ids = inputs.input_ids
+            try:
+                torch.cuda.synchronize()
+                start_time = time.time()
+                output_ids, new_token, step, accept_length_tree = forward_func(
+                    inputs,
+                    model,
+                    tokenizer,
+                    max_new_tokens,
+                    **kwargs,
+                )
+                torch.cuda.synchronize()
+                total_time = time.time() - start_time
+                output_ids = output_ids[0][len(input_ids[0]):]
+                # be consistent with the template's stop_token_ids
+                if conv.stop_token_ids:
+                    stop_token_ids_index = [
+                        i
+                        for i, id in enumerate(output_ids)
+                        if id in conv.stop_token_ids
+                    ]
+                    if len(stop_token_ids_index) > 0:
+                        output_ids = output_ids[: stop_token_ids_index[0]]
+
+                output = tokenizer.decode(
+                    output_ids,
+                    spaces_between_special_tokens=False,
+                )
+                if conv.stop_str and output.find(conv.stop_str) > 0:
+                    output = output[: output.find(conv.stop_str)]
+                for special_token in tokenizer.special_tokens_map.values():
+                    if isinstance(special_token, list):
+                        for special_tok in special_token:
+                            output = output.replace(special_tok, "")
+                    else:
+                        output = output.replace(special_token, "")
+                output = output.strip()
+
+                if conv.name == "xgen" and output.startswith("Assistant:"):
+                    output = output.replace("Assistant:", "", 1).strip()
+                turns.append(output)
+                steps.append(int(step))
+                new_tokens.append(int(new_token))
+                wall_time.append(total_time)
+                conv.messages[-1][-1] = output
+            except RuntimeError as e:
+                print("ERROR question ID: ", question["question_id"])
+                output = "ERROR"
+
+    print('Warmup done')
+
+    accept_lengths_tree = []
+    for question in tqdm(questions):
+
+        choices = []
+        for i in range(num_choices):
+            cur_accept_lengths_tree = []
+            torch.manual_seed(i)
+            conv = get_conversation_template("vicuna")
+
+            # conv = get_conversation_template("Llama-2-7b")
+            # conv.set_system_message(system_message=system_prompt_mapping["Llama"])
+
+            turns = []
+            steps = []
+            new_tokens = []
+            wall_time = []
+            for j in range(len(question["turns"])):
+                qs = question["turns"][j]
+                conv.append_message(conv.roles[0], qs)
+                conv.append_message(conv.roles[1], None)
+                conv.stop_str = "</s>"
+                prompt = conv.get_prompt()
+                inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
+                input_ids = inputs.input_ids
+                try:
+                    torch.cuda.synchronize()
+                    start_time = time.time()
+                    output_ids, new_token, step, accept_length_tree = forward_func(
+                        inputs,
+                        model,
+                        tokenizer,
+                        max_new_tokens,
+                        **kwargs,
+                    )
+                    torch.cuda.synchronize()
+                    total_time = time.time() - start_time
+                    accept_lengths_tree.extend(accept_length_tree)
+                    output_ids = output_ids[0][len(input_ids[0]):]
+
+                    if conv.stop_token_ids:
+                        stop_token_ids_index = [
+                            i
+                            for i, id in enumerate(output_ids)
+                            if id in conv.stop_token_ids
+                        ]
+                        if len(stop_token_ids_index) > 0:
+                            output_ids = output_ids[: stop_token_ids_index[0]]
+
+                    output = tokenizer.decode(
+                        output_ids,
+                        spaces_between_special_tokens=False,
+                    )
+                    if conv.stop_str and output.find(conv.stop_str) > 0:
+                        output = output[: output.find(conv.stop_str)]
+                    for special_token in tokenizer.special_tokens_map.values():
+                        if isinstance(special_token, list):
+                            for special_tok in special_token:
+                                output = output.replace(special_tok, "")
+                        else:
+                            output = output.replace(special_token, "")
+                    output = output.strip()
+
+                    if conv.name == "xgen" and output.startswith("Assistant:"):
+                        output = output.replace("Assistant:", "", 1).strip()
+                except RuntimeError as e:
+                    print("ERROR question ID: ", question["question_id"])
+                    output = "ERROR"
+
+                turns.append(output)
+                steps.append(int(step))
+                new_tokens.append(int(new_token))
+                wall_time.append(total_time)
+                cur_accept_lengths_tree.extend(accept_length_tree)
+                conv.messages[-1][-1] = output
+            # torch.cuda.empty_cache()
+            choices.append({"index": i, "turns": turns, "decoding_steps": steps, "new_tokens": new_tokens, "wall_time": wall_time,
+                            "accept_lengths": cur_accept_lengths_tree})
+
+        # Dump answers
+        os.makedirs(os.path.dirname(answer_file), exist_ok=True)
+        with open(os.path.expanduser(answer_file), "a") as fout:
+            ans_json = {
+                "question_id": question["question_id"],
+                "category": question["category"] if 'category' in question else "None",
+                "answer_id": shortuuid.uuid(),
+                "model_id": model_id,
+                "choices": choices,
+                "tstamp": time.time(),
+            }
+            fout.write(json.dumps(ans_json) + "\n")
+    calculate_metric(answer_file)
+
+def calculate_metric(answer_file):
+    data = []
+    with open(os.path.expanduser(answer_file), 'r') as f:
+        for line in f:
+            data.append(json.loads(line))
+    speeds = []
+    accept_lengths_list = []
+    for datapoint in data:
+        tokens=sum(datapoint["choices"][0]['new_tokens'])
+        times = sum(datapoint["choices"][0]['wall_time'])
+        if isinstance(datapoint["choices"][0]['accept_lengths'][0], int):
+            accept_lengths_list.extend(datapoint["choices"][0]['accept_lengths'])
+        else:
+            accept_lengths_list.extend([d[0] for d in datapoint["choices"][0]['accept_lengths']])
+        speeds.append(tokens/times)
+    print("Token / Sec : {}".format(round(np.mean(speeds), 2)))
+    print("#Mean accepted tokens: {}".format(round(np.mean(accept_lengths_list),2)))
+
+
+def reorg_answer_file(answer_file):
+    """Sort by question id and de-duplication"""
+    answers = {}
+    with open(answer_file, "r") as fin:
+        for l in fin:
+            qid = json.loads(l)["question_id"]
+            answers[qid] = l
+
+    qids = sorted(list(answers.keys()))
+    with open(answer_file, "w") as fout:
+        for qid in qids:
+            fout.write(answers[qid])
+
---
+++ evaluation/inference_hierachy.py
@@ -0,0 +1,181 @@
+"""Generate answers with local models.
+
+Usage:
+python3 gen_model_answer.py --model-path lmsys/fastchat-t5-3b-v1.0 --model-id fastchat-t5-3b-v1.0
+"""
+# adapted from fastchat: https://github.com/lm-sys/FastChat/blob/main/fastchat/llm_judge/gen_model_answer.py
+import argparse
+import os
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from fastchat.utils import str_to_torch_dtype
+
+from evaluation.eval import run_eval, reorg_answer_file
+
+from model.hierachy.utils import augment_all, config_lade
+from model.hierachy.decoding import CONFIG_MAP, set_memory
+
+def hierachy_forward(inputs, model, tokenizer, max_new_tokens, do_sample, temperature, top_k, top_p):
+    model_inputs = inputs.to("cuda")
+    output_ids, idx, accept_length_list, = model.generate(
+        **model_inputs,
+        do_sample=do_sample,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+        max_new_tokens=max_new_tokens,
+    )
+    new_token = len(output_ids[0][len(model_inputs.input_ids[0]):])
+    return output_ids, new_token, idx+1, accept_length_list
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        type=str,
+        required=True,
+        help="The path to the weights. This can be a local folder or a Hugging Face repo ID.",
+    )
+    parser.add_argument(
+        "--model-id", type=str, required=True, help="A custom name for the model."
+    )
+    parser.add_argument(
+        "--bench-name",
+        type=str,
+        default="mt_bench",
+        help="The name of the benchmark question set.",
+    )
+    parser.add_argument(
+        "--question-begin",
+        type=int,
+        help="A debug option. The begin index of questions.",
+    )
+    parser.add_argument(
+        "--question-end", type=int, help="A debug option. The end index of questions."
+    )
+    parser.add_argument("--answer-file", type=str, help="The output answer file.")
+    parser.add_argument(
+        "--max-new-tokens",
+        type=int,
+        default=1024,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--num-choices",
+        type=int,
+        default=1,
+        help="How many completion choices to generate.",
+    )
+    parser.add_argument(
+        "--num-gpus-per-model",
+        type=int,
+        default=1,
+        help="The number of GPUs per model.",
+    )
+    parser.add_argument(
+        "--num-gpus-total", type=int, default=1, help="The total number of GPUs."
+    )
+    parser.add_argument(
+        "--level",
+        type=int,
+        default=3,
+    )
+    parser.add_argument(
+        "--window",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--guess",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        default="float16",
+        choices=["float32", "float64", "float16", "bfloat16"],
+        help="Override the default dtype. If not set, it will use float16 on GPU.",
+    )
+
+    parser.add_argument(
+        "--do_sample",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--temperature", 
+        type=float,
+        default=0.7
+    )
+    parser.add_argument(
+        "--top_k",
+        type=int,
+        default=50
+    )
+    parser.add_argument(
+        "--top_p",
+        type=float,
+        default=0.9
+    )
+
+
+    # Memory Setting
+    parser.add_argument("--do_WM", action="store_true")
+    parser.add_argument("--do_SM", action="store_true")
+    parser.add_argument("--do_LM", action="store_true")
+    parser.add_argument("--order", type=str, default="WSL")
+    parser.add_argument("--history_file", type=str, default="train-Llama-2-7b-history2.json")
+    parser.add_argument("--db_file", type=str, default="datastore_chat_large.idx")
+    parser.add_argument("--previous_tokens", type=int, default=8)
+    parser.add_argument("--system_prompt", type=str, default="default")
+
+
+    args = parser.parse_args()
+    if int(os.environ.get("USE_LADE", 0)):
+        augment_all()
+        config_lade(LEVEL=args.level, WINDOW_SIZE=args.window, GUESS_SET_SIZE=args.guess, DEBUG=0,
+                         USE_FLASH=0, DIST_WORKERS=len(os.environ.get("CUDA_VISIBLE_DEVICES").split(",")), DO_WM=args.do_WM, DO_SM=args.do_SM, DO_LM=args.do_LM, ORDER=args.order, PREVIOUS_TOKENS=args.previous_tokens)
+        print("hierahcy activated config: ", CONFIG_MAP)
+
+    question_file = f"data/{args.bench_name}/question.jsonl"
+    if args.answer_file:
+        answer_file = args.answer_file
+    else:
+        answer_file = f"data/{args.bench_name}/model_answer/{args.model_id}.jsonl"
+
+    print(f"Output to {answer_file}")
+
+    set_memory(args)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path,
+        torch_dtype=str_to_torch_dtype(args.dtype),
+        low_cpu_mem_usage=True,
+        device_map="auto"
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+
+    run_eval(
+        model=model,
+        tokenizer=tokenizer,
+        forward_func=hierachy_forward,
+        model_id=args.model_id,
+        question_file=question_file,
+        question_begin=args.question_begin,
+        question_end=args.question_end,
+        answer_file=answer_file,
+        max_new_tokens=args.max_new_tokens,
+        num_choices=args.num_choices,
+        num_gpus_per_model=args.num_gpus_per_model,
+        num_gpus_total=args.num_gpus_total,
+        do_sample=args.do_sample,
+        temperature=args.temperature,
+        top_k=args.top_k,
+        top_p=args.top_p,
+        system_prompt=args.system_prompt
+    )
+
+    reorg_answer_file(answer_file)
